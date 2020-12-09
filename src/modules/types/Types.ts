7fb81215333abb16dafb27e383336ad15b4f9ab7
Brendan Lane <me@imbl.me>
2020-12-09 02:34:30 -0500

    💻 Sync with laptop

--- src/modules/types/Types.ts
+++ src/modules/types/Types.ts
@@ -1,10 +1,11 @@
 // SpookVooper API - modules/types/Types.ts
 // Written by Brendan Lane - https://brndnln.dev/
 
-import { EntityUser } from './../interfaces/Interfaces'
+import { EntityUser, AuthConfig as AuthConfigInt } from './../interfaces/Interfaces'
 
 export type CreditAmount = string | number
 export type PaymentEntity = string | EntityUser
 export type SVStockTicker = 'B' | 'IDE' | 'NEWS' | 'POT' | 'TECH' | 'TYCO' | 'VC' | 'VNB' | 'VU' | 'X'
 export type AuthEntity = EntityUser
 export type GroupMember = string | EntityUser
+export type AuthConfig = AuthConfigInt
