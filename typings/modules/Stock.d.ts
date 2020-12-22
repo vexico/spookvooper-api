e7646893b054732cff1fc165c2924090b6559bc8
Brendan Lane <me@brndnln.dev>
2020-12-22 00:29:13 -0500

    💵 Finish ExchangeHub Fixes #16

---
+++ typings/modules/Stock.d.ts
@@ -0,0 +1,12 @@
+import { QueueType } from './types/Types'
+declare class Stock {
+  private readonly stockTicker
+  get ticker (): string;
+  set ticker (ticker: string);
+  constructor (ticker: string);
+  getValue (): Promise<any>;
+  getBuyPrice (): Promise<any>;
+  getQueueInfo (type: QueueType): Promise<any>;
+  getOwnerData (): Promise<any>;
+}
+export default Stock
